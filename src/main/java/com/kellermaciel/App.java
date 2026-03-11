9f8790a5d85b71212479f4b207479931beb2e88b
KellerBeMaciel <kellerbe.maciel@gmail.com>
2026-03-11 19:41:21 -0300

    Atividade Módulo 8 - Estruturas de Repetição

--- src/main/java/com/kellermaciel/App.java
+++ src/main/java/com/kellermaciel/App.java
@@ -1,49 +1,60 @@
 package com.kellermaciel;
-
 import java.util.Scanner;
 
 public class App
 {
     public static void main( String[] args )
     {
+        int input;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Greetings! \nTemos algumas perguntas para você. \nVamos Começar!");
-
-        System.out.println("Digite seu nome completo: ");
+        System.out.print("Greetings! \nPor favor, informe o seu nome: ");
         String nome = scanner.nextLine();
 
-        System.out.println("Digite seu CPF:");
-        String cpf = scanner.nextLine();
+        do {
+            System.out.print("\nAgora, informe um número inteiro: ");
+            input = scanner.nextInt();
+            scanner.nextLine();
 
-        System.out.println("Digite sua Idade: ");
-        int idade = scanner.nextInt();
-        scanner.nextLine();
+            if(input > 0) break;
 
-        System.out.println("Digite seu peso: ");
-        double peso = scanner.nextDouble();
-        scanner.nextLine();
+            System.out.println("O número " + input + " é inválido, por favor, informe um inteiro positivo maior que zero.");
+        } while (true);
 
-        System.out.println("Digite sua altura: ");
-        double altura = scanner.nextDouble();
-        scanner.nextLine();
+        imprimirContagemCrescente(input);
+        imprimirContagemDecrescente(input);
+        imprimirNome(nome, input);
 
-        System.out.println("Digite seu Estado Civil: ");
-        String estadoCivil = scanner.nextLine();
-
-        System.out.println("\nObrigado pela sua participação, dados registrados: ");
-
-        String dados =  "============================================================================\n" +
-                        String.format("Nome Completo: %s \n", nome) +
-                        String.format("CPF:: %s          \n", cpf) +
-                        String.format("Idade: %d         \n", idade) +
-                        String.format("Peso: %f          \n", peso) +
-                        String.format("Altura: %f        \n", altura) +
-                        String.format("Estado Civil: %s  \n", estadoCivil) +
-                        "============================================================================\n";
-
-        System.out.println(dados);
         scanner.close();
-
     }
+
+    public static void imprimirContagemCrescente(int numeroInformado){
+        adicionarTitulo("Contagem Crescente");
+
+        for(int i = 0; i <= numeroInformado; i++){
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void imprimirContagemDecrescente(int numeroInformado){
+        adicionarTitulo("Contagem Decrescente");
+
+        while(numeroInformado >= 0){
+            System.out.print(numeroInformado-- + " ");
+        }
+    }
+
+    public static void imprimirNome(String nome, int numeroInformado){
+        adicionarTitulo("Obrigado!");
+        int numerosImpressao = nome.length() > 6 ? numeroInformado : 1;
+
+        for(int i = 0; i < numerosImpressao; i++){
+            System.out.println(nome.toUpperCase() + "!");
+        }
+    }
+
+    public static void adicionarTitulo(String titulo){
+        System.out.println("\n===== " + titulo.toUpperCase() + " =====");
+    }
+
 }
