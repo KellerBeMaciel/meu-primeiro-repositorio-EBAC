a87a83b4ad5f5dbcc90d1e80db2cc355776df1b4
KellerBeMaciel <kellerbe.maciel@gmail.com>
2026-03-11 20:33:56 -0300

    Atividade Módulo 9 - Estruturas de Repetição 2

--- src/main/java/com/kellermaciel/App.java
+++ src/main/java/com/kellermaciel/App.java
@@ -4,46 +4,75 @@ import java.util.Scanner;
 
 public class App
 {
-    public static void main( String[] args )
-    {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Greetings! \nTemos algumas perguntas para você. \nVamos Começar!");
+    static Scanner scanner;
 
-        System.out.println("Digite seu nome completo: ");
-        String nome = scanner.nextLine();
+    public static void main( String[] args ) {
+        scanner =  new Scanner(System.in);
 
-        System.out.println("Digite seu CPF:");
-        String cpf = scanner.nextLine();
+        int numero = getNumeroInteiroPositivo();
+        imprimirTabuada(numero);
 
-        System.out.println("Digite sua Idade: ");
-        int idade = scanner.nextInt();
-        scanner.nextLine();
-
-        System.out.println("Digite seu peso: ");
-        double peso = scanner.nextDouble();
-        scanner.nextLine();
-
-        System.out.println("Digite sua altura: ");
-        double altura = scanner.nextDouble();
-        scanner.nextLine();
-
-        System.out.println("Digite seu Estado Civil: ");
-        String estadoCivil = scanner.nextLine();
-
-        System.out.println("\nObrigado pela sua participação, dados registrados: ");
-
-        String dados =  "============================================================================\n" +
-                        String.format("Nome Completo: %s \n", nome) +
-                        String.format("CPF:: %s          \n", cpf) +
-                        String.format("Idade: %d         \n", idade) +
-                        String.format("Peso: %f          \n", peso) +
-                        String.format("Altura: %f        \n", altura) +
-                        String.format("Estado Civil: %s  \n", estadoCivil) +
-                        "============================================================================\n";
-
-        System.out.println(dados);
+        iniciarPrograma();
         scanner.close();
+    }
 
+    public static void iniciarPrograma() {
+        do {
+            System.out.println( "Opção 1: Continuar. \n" +
+                                "Opção 2: Sair.");
+
+            int selecao = 0;
+            String input = scanner.nextLine();
+
+            try {
+                selecao = Integer.parseInt(input);
+            } catch (Exception e){
+                System.out.println("Entrada " + input + " é inválida.");
+                continue;
+            }
+
+            switch (selecao) {
+                case 1:
+                    repetirFrase();
+                    break;
+                case 2:
+                    System.out.println("Obrigado por utilizar o nosso sistema, espero que tenha gostado.");
+                    return;
+                default:
+                    System.out.println("Opção inválida.");
+            }
+        } while (true);
+    }
+
+    public static void repetirFrase(){
+        System.out.println("Digite o que quiser! ");
+        String input = scanner.nextLine();
+
+        System.out.println("$ " + input);
+    }
+
+    public static int getNumeroInteiroPositivo(){
+        do{
+            System.out.println("Digite um número inteiro:");
+            int input = scanner.nextInt();
+            scanner.nextLine();
+
+            if(input > 0) return input;
+
+            System.out.println("O número " + input + " é inválido, informe um número positivo maior que 0.");
+        } while(true);
+    }
+
+    public static void imprimirTabuada(int numeroInformado){
+        System.out.println("== Tabuada de " + numeroInformado + " ==");
+        int multiplicador = 1;
+        int resultado = 0;
+
+        while(multiplicador <= 10){
+            resultado = resultado + numeroInformado;
+            System.out.println(numeroInformado + " \tx " + multiplicador + " \t= " + resultado);
+            multiplicador++;
+        }
     }
 }
