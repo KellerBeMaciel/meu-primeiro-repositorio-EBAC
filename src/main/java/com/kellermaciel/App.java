79872e31cb1bcb7c8ad022d312f6c8a233a2d24f
KellerBeMaciel <kellerbe.maciel@gmail.com>
2026-03-11 19:08:26 -0300

    Atividade Módulo 7 - Condicionais

--- src/main/java/com/kellermaciel/App.java
+++ src/main/java/com/kellermaciel/App.java
@@ -8,42 +8,24 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Greetings! \nTemos algumas perguntas para você. \nVamos Começar!");
+        System.out.println("Greetings!");
 
-        System.out.println("Digite seu nome completo: ");
-        String nome = scanner.nextLine();
-
-        System.out.println("Digite seu CPF:");
-        String cpf = scanner.nextLine();
-
-        System.out.println("Digite sua Idade: ");
-        int idade = scanner.nextInt();
+        System.out.print("Informe um número inteiro: ");
+        int primeiroNumero = scanner.nextInt();
         scanner.nextLine();
 
-        System.out.println("Digite seu peso: ");
-        double peso = scanner.nextDouble();
+        System.out.print("Agora, informe um segundo número inteiro: ");
+        int segundoNumero = scanner.nextInt();
         scanner.nextLine();
 
-        System.out.println("Digite sua altura: ");
-        double altura = scanner.nextDouble();
-        scanner.nextLine();
+        if(primeiroNumero > segundoNumero) {
+            System.out.println("O maior número é: " + primeiroNumero);
+        } else if (segundoNumero > primeiroNumero) {
+            System.out.println("O maior número é: " + segundoNumero);
+        } else {
+            System.out.println("Os números são iguais!");
+        }
 
-        System.out.println("Digite seu Estado Civil: ");
-        String estadoCivil = scanner.nextLine();
-
-        System.out.println("\nObrigado pela sua participação, dados registrados: ");
-
-        String dados =  "============================================================================\n" +
-                        String.format("Nome Completo: %s \n", nome) +
-                        String.format("CPF:: %s          \n", cpf) +
-                        String.format("Idade: %d         \n", idade) +
-                        String.format("Peso: %f          \n", peso) +
-                        String.format("Altura: %f        \n", altura) +
-                        String.format("Estado Civil: %s  \n", estadoCivil) +
-                        "============================================================================\n";
-
-        System.out.println(dados);
         scanner.close();
-
     }
 }
